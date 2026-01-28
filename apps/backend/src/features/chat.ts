4550141d537b1bd92231bb82db27e55682a4d2b1
Aayushman Pratap <aayushmanpratap333@gmail.com>
2026-01-28 17:36:00 +0530

    added env bindings and used google provider for getting ai res

--- apps/backend/src/features/chat.ts
+++ apps/backend/src/features/chat.ts
@@ -1,28 +1,41 @@
 import { streamText } from "ai";
-import { Hono } from "hono";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import {  Hono } from "hono";
 import { ERRRO_MESSAGES } from "../../constants/errorCodes";
 import { MessageSchema } from "../utils/schemas/chat";
 import { SYSTEM_PROMPT } from "../../constants/systemPrompt";
 
-export const chatRoute = new Hono();
+export const chatRoute = new Hono<{ Bindings: Cloudflare.Env }>();
+
 
 chatRoute.post('/chat', async (c) => {
+  try{
+
+    const google = createGoogleGenerativeAI({
+      apiKey: c.env.GOOGLE_AI_API_KEY,
+    })
+
     const payload = await c.req.json();
     const validatedPayload = MessageSchema.safeParse(payload);
-    
+
     if (!validatedPayload.success) {
       return c.json({ok: false, error: validatedPayload.error.message, errorMessage: ERRRO_MESSAGES.INVALID_PAYLOAD}, 400);
     }
-  
+
     const userPrompt = validatedPayload.data.message;
-  
+    
     const message = streamText({
-      model:"google/gemini-2.5-flash",
-      prompt:userPrompt,
+      model: google("gemini-3-flash-preview"),
+      prompt:userPrompt,  
       system:SYSTEM_PROMPT,
-    })
-  
-    return message.toUIMessageStreamResponse()
+    });
+    
+    return message.toUIMessageStreamResponse();
+  }
+
+  catch(error){
+    return c.json({ok: false, error: error instanceof Error ? error.message : "An unknown error occurred", errorMessage: ERRRO_MESSAGES.INTERNAL_SERVER_ERROR}, 500);
+  }
   })
 
 
