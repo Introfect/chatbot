11f55242ecfb24df81fe9ea38df74586ebd1d6eb
Aayushman Pratap <aayushmanpratap333@gmail.com>
2026-03-01 19:25:50 +0530

    integrate langfuse

--- apps/backend/src/utils/utils.ts
+++ apps/backend/src/utils/utils.ts
@@ -1,7 +1,33 @@
+import { ModelMessage } from "ai";
 import { SYSTEM_PROMPT } from "../../constants/systemPrompt";
 
 export function getSystemPrompt({ slug }: { slug: string | null }) {
     return slug
         ? `${SYSTEM_PROMPT}\n\nThe user is currently viewing the blog post with slug: "${slug}". When they ask about "this article" or "the blog", use blog_search with this slug.`
         : SYSTEM_PROMPT;
+}
+
+
+export function getUserMessage(lastMessage: ModelMessage) {
+    if (lastMessage.role !== "user") {
+        return null;
+    }
+
+    const lastMessageContent = lastMessage.content;
+
+    if (typeof lastMessageContent === "string") {
+        return lastMessageContent;
+    }
+
+    const lastPart = lastMessageContent.at(-1);
+
+    if (!lastPart) {
+        throw new Error("User message content is empty");
+    }
+
+    if (lastPart.type === "text") {
+        return lastPart.text;
+    }
+
+    return null;
 }
